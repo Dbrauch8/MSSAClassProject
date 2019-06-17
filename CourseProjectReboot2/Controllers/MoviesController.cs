60ed16f63ef8ae5d9ec5674ea2b212477c4c5ea0
Doug Brauch <doug.c.brauch@outlook.com>
2019-06-17 13:02:36 -0400

    Add project files.

---
+++ CourseProjectReboot2/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseProjectReboot2.Models;
+
+namespace CourseProjectReboot2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name =  "Shreck!" };
+
+            //return View(movie);
+            //return Content("Hello World");
+            return HttpNotFound();
+        }
+    }
+}
